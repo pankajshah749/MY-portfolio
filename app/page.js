a9bda1ff503391f8339e2829e4d3efe1768363b6
pankajshah749 <sahpankaj749@gmail.com>
2024-12-17 16:31:22 +0545

    remove experience

--- app/page.js
+++ app/page.js
@@ -13,7 +13,7 @@ export default async function Home() {
     <>
       <HeroSection />
       <AboutSection />
-      <Experience />
+      {/* <Experience /> */}
       <Skills />
       <Projects />
       <Education />
