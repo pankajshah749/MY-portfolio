787e41ec90fe91d2acd91ad5cfadcd94efedef29
pankajshah749 <sahpankaj749@gmail.com>
2024-12-17 15:54:35 +0545

    make glow card as dynamic component

--- app/components/homepage/education/index.jsx
+++ app/components/homepage/education/index.jsx
@@ -1,10 +1,13 @@
 // @flow strict
 import Image from "next/image";
+import dynamic from 'next/dynamic';
+
+const GlowCard = dynamic(() => import('../../helper/glow-card'), { ssr: false });
+
 
 import { educations } from "@/utils/data/educations";
 import { BsPersonWorkspace } from "react-icons/bs";
 import AnimationLottie from "../../helper/animation-lottie";
-import GlowCard from "../../helper/glow-card";
 import lottieFile from '/public/lottie/study.json';
 
 function Education() {
